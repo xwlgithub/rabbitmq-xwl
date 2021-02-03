6a7915893c81be4068117e559e7a6b7eacb0bad8
xwlgithub <2509647976@qq.com>
2021-02-03 17:32:06 +0800

    需求确认

--- order-server/src/main/java/com/xwl/controller/OrderController.java
+++ order-server/src/main/java/com/xwl/controller/OrderController.java
@@ -1,15 +1,8 @@
 package com.xwl.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @Auther: 薛
@@ -20,30 +13,17 @@ import java.util.stream.Collectors;
 @RequestMapping("xwl")
 @AllArgsConstructor
 public class OrderController {
-//    class User{
-//        private String name;
-//        private String value;
-//
-//        public String getName() {
-//            return name;
-//        }
-//
-//        public void setName(String name) {
-//            this.name = name;
-//        }
-//
-//        public String getValue() {
-//            return value;
-//        }
-//
-//        public void setValue(String value) {
-//            this.value = value;
-//        }
-//    }
-//    public static void main(String[] args) {
-//        List<User> userList=new LinkedList<>();
-//        List<Map<String,Object>> objects = new LinkedList<>();
-//        Map<String, Object> maps = userList.stream().collect(Collectors.toMap(user -> user.getName(), userTwo -> userTwo.getValue()));
-//        objects.add(maps);
-//    }
+
+    /**
+     * 举例说明：订单表,库存表
+     * 1.根据当前商品信息类型及其它字段查询库存表是否有库存
+     *      如果有及创建订单       反之返回死信队列统一返回商品已抢空
+     *            <I>
+     *                订单已创建之后,更新库存信息,也就是减库存
+     *                      <I>
+     *                          如果订单创建成功,更新库存信息,(如果库存更新失败,即监听器无法确认消费消息,启动重试机制(新建一个补单队列))
+     *                      </I>
+     *            </I>
+     */
+
 }
